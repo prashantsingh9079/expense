34a76e9217cbd61da1059e2b13751ea3799b22ca
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-04 09:48:08 +0530

    added location filed and basic styling in expense tracker

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,12 +1,18 @@
+import './Item.css'
 function Items()
 {
+    const expenseDate = new Date(2023,5,4);
+    const expenseDesc = "Car Insurance";
+    const expensePrice = 8000;
+    const loc = "Faridabad";
     return (
-        <div>
-            <ul>
-                <li>Food    Rs 10</li>
-                <li>Petrol  Rs 100</li>
-                <li>Movies  Rs 200</li>
-            </ul>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseDesc}</h2>
+                <h2>{loc}</h2>
+                <div className="expense-item__price"> Rs {expensePrice}</div>
+            </div>
         </div>
     )
 }

cae55e359f8fec01873fc3a10d65f5f68e5af27b
prashantsingh9079 <prashant.singh9079@gmail.com>
2023-06-04 08:14:26 +0530

    first commit added listmitems by hard code

---
+++ src/Components/Items.js
@@ -0,0 +1,14 @@
+function Items()
+{
+    return (
+        <div>
+            <ul>
+                <li>Food    Rs 10</li>
+                <li>Petrol  Rs 100</li>
+                <li>Movies  Rs 200</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Items;
